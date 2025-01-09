d4ea2d92c0ae03fd90b85bd84f66b31735bdef43
ningbbang <wogh2209@naver.com>
2025-01-10 01:49:28 +0900

    modified

--- server.js
+++ server.js
@@ -14,13 +14,17 @@ log_db.loadDatabase();
 places_db.loadDatabase();
 plan_db.loadDatabase();
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
 app.use(express.static('public'));
 app.use(express.json());
-app.post('/add-item', (req, res) => {
+
+app.post('/add-item/:category', (req, res) => {
+    const category = req.params.category;
     const data = req.body;
     places_db.insert(data);
-    console.log('item added :', data)
     res.json(data);
+    // return res.redirect(`/list-page/${category}`)
 });
 
 app.get('/overview', (req, res) => {
@@ -51,6 +55,62 @@ app.get('/plan', (req, res) => {
     });
 });
 
+app.get('/api/restaurant', (req, res) => {
+    places_db.find({category:'restaurant'}, (err, data) => {
+        if (err) {
+            response.end();
+            return;
+        }
+        res.json({data});
+    })
+});
+
+app.get('/api/museum', (req, res) => {
+    places_db.find({category:'museum'}, (err, data) => {
+        if (err) {
+            response.end();
+            return;
+        }
+        res.json({data});
+    })
+});
+
+app.get('/api/activity', (req, res) => {
+    places_db.find({category:'activity'}, (err, data) => {
+        if (err) {
+            response.end();
+            return;
+        }
+        res.json({data});
+    })
+});
+
+app.get('/api/hotel', (req, res) => {
+    places_db.find({category:'hotel'}, (err, data) => {
+        if (err) {
+            response.end();
+            return;
+        }
+        res.json({data});
+    })
+});
+
+app.get('/list-page/:category', (req, res) => {
+    const category = req.params.category;
+
+    places_db.find({category: category}, (err, data) => {
+        if (err) {
+            res.status(500).send('Error retrieving data');
+            return;
+        }
+        res.render('list-page', { category, data });
+    });
+})
+
+app.get('/add-item/:category', (req, res) => {
+    const category = req.params.category;
+    res.render('add-item', { category });
+})
 
 app.listen(port, () => {
     console.log(`app is running on http://localhost:${port}`)
